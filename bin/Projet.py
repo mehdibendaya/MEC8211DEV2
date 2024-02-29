93046176717e0726f02f75e0d0d4fa63f88105c0
Mbd <bendaya.mehdi@gmail.com>
2024-02-28 20:37:13 -0500

    V2

--- bin/Projet.py
+++ bin/Projet.py
@@ -24,6 +24,7 @@ import matplotlib.pyplot as plt
 from Fonctions import *
 from Tests_unitaires import *
 import unittest
+import csv
 
 
 N_THREADS = '12'
@@ -44,91 +45,49 @@ class param():
     #Pour regime transitoire    
     dr=0.005  #Pas en espace
     dt=0.5*dr**2/(D_eff*10) # Pas en temps
-    n=int(R/dr)
+    n=int(R/dr)+1
     err_t_tdt=10e-7 #Condition d'arret 
     k=4e-9   
-    tf=10000000
+    tf=10e6
     
-prm=param()
-r=np.linspace(0,prm.R,prm.n)
-C_num,tps=PbF(prm)
-plt.plot(r,C_num)
-plt.grid()
 
 '''# ==========================================================================
 # =============================================================================
-# =====================  Deuxieme cas : Schema d'ordre 2  =====================
+# ============================  Schema d'ordre 2  =============================
 # =============================================================================
 # =============================================================================
 
 # ============================================================================= 
-# ============================= Regime transitoire ============================
+# ============================= Vérification code à code ======================
 # ==========================================================================''' 
     
-def PartieF():    
+def Cac():    
      
-    # Initialisation des paramètres'
-    prm = param()
-    dr_testee = [0.1,0.04,0.02,0.01,0.005]   
-      
-    # Initialisation des vecteurs
-    erreur_L1 = []
-    erreur_L2 = []
-    erreur_Linf = []
-    c_ordre2= []
-    r_ordre2= []   
-    
-    # Calcul de de l'erreur
-    for dr_act in dr_testee:
-       
-        prm.dr = dr_act
-        prm.n  = int(prm.R/prm.dr)
-        print(prm.dr)
-        r,C_analy=C_analytique(prm)
-        C_num,tps=PbF(prm)
-        
-        c_ordre2.append(C_num)
-        r_ordre2.append(r)
-        
-        epsilon_h=C_num-C_analy
-        
-        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n)
-        erreur_L2.append(np.linalg.norm(epsilon_h))
-        erreur_Linf.append(max(abs(epsilon_h)))
-        
-      
-    print("Erreur L2")
-    for i in range(len(dr_testee)):
-        print(dr_testee[i],erreur_L2[i])
-    for i in range(len(dr_testee)-1):    
-        print(log(erreur_L2[i-1]/erreur_L2[i])/log(dr_testee[i-1]/dr_testee[i]))
-    
-    # Graphiques
-    plt.figure(1)
-    plt.rcParams['text.usetex'] = True
-    plt.loglog(dr_testee,erreur_L1,'.',label='$L_{1}$')  
-    plt.loglog(dr_testee,erreur_L2,'.',label='$L_{2}$')
-    plt.loglog(dr_testee,erreur_Linf,'.',label='$L_{\infty}$')
-    plt.gca().invert_xaxis()
-    plt.xlabel("$\delta$r")
-    plt.ylabel("Erreur")
-    plt.legend()
-    plt.title("Convergence de l'erreur  en fonction de $\Delta$r")
-    plt.savefig('Ordre2_Conv_dr', dpi=1000)
-
-    plt.figure(2)
-    for i in range(len(c_ordre2)):
-        if dr_testee[i] in [0.25,0.0002,0.0001]:
-            lab='dr='+str(dr_testee[i])
-            plt.plot(r_ordre2[i],c_ordre2[i],'-.',label=lab)
-    plt.plot(r,C_analy,'-.',label="Sol analytique",linewidth=1.1)       
-    plt.legend()
+    prm=param()
+    r=np.linspace(0,prm.R,prm.n)
+    prm.tf=tf=10e6
+    C_num1,tps1=PbF(prm)
+    # prm.tf=10e8
+    # C_num2,tps2=PbF(prm)
+    # prm.tf=10e9
+    # C_num3,tps3=PbF(prm)
+    plt.plot(r,C_num1)
     plt.grid()
-    plt.xlabel("r [$m$]")
-    plt.ylabel("C [mol/$m^{3}$]")
-    plt.title("Profil de concentration en fonction de $\Delta$r")
-    plt.savefig('Ordre2_C_dr', dpi=1000)
     
+    with open('10e7_D.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for element2, element1 in zip(C_num1, r):
+            writer.writerow([element1, element2])
+    with open('10e8_D.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for element2, element1 in zip(C_num1, r):
+            writer.writerow([element1, element2])
+    with open('10e9_D.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for element2, element1 in zip(C_num1, r):
+            writer.writerow([element1, element2])
+    print(prm.dt)
 
 print("Veuillez attendre la vérification du code est en cours.")    
 unittest.main(module=__name__)  
+Cac()
